4c498ee13e136192b07b57d089ca841b22d2a276
Evan Armstrong <earmstrong@lcs.on.ca>
2021-01-29 09:41:14 -0500

    Almost have number system converter working.

--- NumberSystemConverter/main.swift
+++ NumberSystemConverter/main.swift
@@ -8,70 +8,63 @@
 import Foundation
 
 // INPUT
-
-// Get the "from" number system
-print("What number system do you want to convert from?")
-print("B: Binary")
-print("O: Octal")
-print("D: Decimal")
-print("H: Hexadecimal")
-print("Enter your choice(B/O/D/H): ", terminator: "") // Keep following output
-                                                      // On the same line
-let from = readLine()!
-print(from)
-
-print("What do you want to convert to?")
-print("B: Binary")
-print("O: Octal")
-print("D: Decimal")
-print("H: Hexadecimal")
-print("Enter your choice(B/O/D/H): ", terminator: "")
-
-let from1 = readLine()
-print(from1!)
-
-print("What number do you want to convert?", terminator: "")
-let value = readLine()
-// PROCESS
-
-
-func
-    let value = Int()
-//
-// What base are we converting from
-let base = 2.0
-//
-// The exponent value at the right most digit
-var exponent = 0.0
-//
-// the current sum in decimal
-var decimalEquivalent = 0.0
-//
-// Iterate over each character
-// From right to left!
-for character in value.reversed() {
-    
-    // Get the current digit as a double (decimal)
-    if let digit = Double(String(character)){
-        
-        // Add the current sum
-       decimalEquivalent += digit * pow (base, exponent)
-        
-        // Increment the exponent
-        exponent += 1
-    }
-                                                      
-}
-// get the value
-decimalEquivalent
-}
-
 enum numberSystemBase: Int {
     case binary = 2   // is the raw value for this case
     case octal = 8   // is the raw value for this case
+    case decimal = 10
     case hexidecimal = 16  // is the raw value for this case
 }
 
+func getDecimalEquivalent(of value: String, from numberSystem: numberSystemBase) -> Double {
+    
+    // What base are we converting from?
+    var base = 0.0
+    switch numberSystem {
+    case .binary:
+        base = 2.0
+    case .octal:
+        base = 8.0
+    case .hexidecimal:
+        base = 16.0
+    case .decimal:
+        base = 10.0
+    }
+    
+    let exponent = 0.0
+    
+    var decimalEquivalent = 0.0
+    
+    for character in value.reversed(){
+        
+        if let digit = Double(String(character)) {
+        
+        decimalEquivalent += digit * pow (base, exponent)
+        
+
+    } else {
+
+        switch character {
+        case "A":
+            decimalEquivalent += 10.0 * pow (base, exponent)
+        case "B":
+            decimalEquivalent += 11.0 * pow (base, exponent)
+        case "C":
+            decimalEquivalent += 12.0 * pow (base, exponent)
+        case "D":
+            decimalEquivalent += 13.0 * pow (base, exponent)
+        case "E":
+            decimalEquivalent += 14.0 * pow (base, exponent)
+        case "F":
+            decimalEquivalent += 15.0 * pow (base, exponent)
+        default:
+            break
+          }
+        
+}
+    }
+        return decimalEquivalent
+}
+   
 func getRepresentation(of ValueToConvert: Int, inBase base: numberSystemBase) -> String  {
     
  
@@ -137,9 +130,73 @@ func getRepresentation(of ValueToConvert: Int, inBase base: numberSystemBase) ->
     }
     return representation
 }
+// What base are we converting from?
 
+// Get the "from" number system
+print("What number system do you want to convert from?")
+print("B: Binary")
+print("O: Octal")
+print("D: Decimal")
+print("H: Hexadecimal")
+print("Enter your choice(B/O/D/H): ", terminator: "") // Keep following output
+                                                      // On the same line
+let from = readLine()!
+print(from)
+
+print("What do you want to convert to?")
+print("B: Binary")
+print("O: Octal")
+print("D: Decimal")
+print("H: Hexadecimal")
+print("Enter your choice(B/O/D/H): ", terminator: "")
+
+let from1 = readLine()
+print(from1!)
+
+print("What number do you want to convert?", terminator: "")
+let value = readLine()
+// PROCESS
+
+// Create a variable that will store the "from" number system in a data type that is compatible with the function
+
+
+
+var fromBase = numberSystemBase.binary
+
+switch from {
+case "B" :
+    fromBase = .binary
+case "O" :
+    fromBase = .octal
+case "D" :
+    fromBase = .decimal
+case "H" :
+    fromBase = .hexidecimal
+    
+default:
+    print("Not a Usable Value")
+}
+
+
+var endBase = numberSystemBase.binary
+switch from {
+case "B" :
+    endBase = .binary
+case "O" :
+    endBase = .octal
+case "D" :
+    endBase = .decimal
+case "H" :
+    endBase = .hexidecimal
+default:
+    print ("Please Enter a Valid Value")
+}
+
+
+let decimal = getDecimalEquivalent(of: value!, from: fromBase)
+let output = getRepresentation(of: Int(decimal), inBase: endBase)
 // Call or use the function
 
-getRepresentation(of: 15, inBase: .hexidecimal)
 // OUTPUT
 
+print(output)
